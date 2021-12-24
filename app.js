67299215e1e74f901ee9721cef9caf9abf3e55ee
jantonio2 <jesusrochavedia@gmail.com>
2021-12-23 21:39:54 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+console.clear();
+console.log('===============');
+console.log('  Tabla del 5');
+console.log('===============');
+
+const num = 5;
+
+for(let i = 1; i <= 10; i++){
+  console.log(`${num} x ${i} = ${num * i}`);
+}
