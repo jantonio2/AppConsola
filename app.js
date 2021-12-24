d7e31f192c78dfdfddd2a5758ae68358b6926127
jantonio2 <jesusrochavedia@gmail.com>
2021-12-24 15:00:52 -0400

    Agregando yargs al proyecto

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const { crearArchivo } = require('./helpers/multiplicar');
+const { argv } = require('yargs');
 
 console.clear();
 
-const [, , arg3 = 'base=5'] = process.argv;
-const [, base = 5] = arg3.split('=');
+// Sin yargs
+// const [, , arg3 = 'base=5'] = process.argv;
+// const [, base = 5] = arg3.split('=');
 
-crearArchivo( base )
-  .then( nombreArchivo => console.log(nombreArchivo, 'creado'))
-  .catch( console.log );
+console.log(process.argv);
+console.log(argv);
+console.log('base: yargs', argv.base);
+
+// crearArchivo( base )
+//   .then( nombreArchivo => console.log(nombreArchivo, 'creado'))
+//   .catch( console.log );
