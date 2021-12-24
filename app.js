0e1655089b80a55f81c5d7f3a88de95debc1d67a
jantonio2 <jesusrochavedia@gmail.com>
2021-12-24 11:33:40 -0400

    Grabando el resultado en un archivo de texto

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
+const { writeFile } = require('fs');
+
 console.clear();
 console.log('===============');
 console.log('  Tabla del 5');
 console.log('===============');
 
-const num = 5;
+const num = 3;
+let salida = '';
 
 for(let i = 1; i <= 10; i++){
-  console.log(`${num} x ${i} = ${num * i}`);
+  salida += `${num} x ${i} = ${num * i}\n`;
 }
+
+console.log(salida);
+
+writeFile(`tabla-${num}.txt`, salida, (err) => {
+  if (err) throw err;
+
+  console.log(`tabla-${num}.txt creado`);
+});
